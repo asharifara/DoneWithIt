a0a2bcb698b79757b552d2662ad1c9251d0109b3
Ali <a.sharifara@gmail.com>
2020-06-06 21:35:11 -0700

    some more update

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {component, useState} from 'react';
+
+import { StyleSheet, SafeAreaView, TouchableOpacity, Text, View, Image, Button } from 'react-native';
 
 export default function App() {
-  console.log("app executed");
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(prevCount => prevCount + 1);
+
   return (
     <View style={styles.container}>
-      <Text> Test App!! </Text>
+      <SafeAreaView style={styles.container}>
+          <Text style={ styles.h1}> Hello! </Text>
+          <View style={styles.countContainer}>
+            <Text>Count: {count}</Text>
+          </View>
+          <TouchableOpacity onPress={onPress}>
+            <Image 
+              blurRadius={0.9}
+              source={{ uri: 'https://picsum.photos/200/300'}} 
+              style={styles.backgroundImage} 
+            />
+          </TouchableOpacity>
+      </SafeAreaView>
     </View>
   );
 }
@@ -13,8 +28,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: '#345',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  backgroundImage : {
+    width: 200,
+    height: 300,
+  },
+  h1: {
+    fontWeight: 'bold',
+    fontSize: 50,
+    color: '#cbb',
+  }
 });
